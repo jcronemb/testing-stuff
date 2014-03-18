5df866d87671bbdf8526d148812c86cefb3935f4
JRC <cr0n3n@hotmail.com>
2014-03-18 13:54:35 -0700

    Initial commit

---
+++ testapp/testproj/src/testapp/Class1.java
@@ -0,0 +1,16 @@
+package testapp;
+
+public class Class1 {
+    public Class1() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+        class1.hello();
+    }
+    
+    public void hello() {
+        System.out.println("Hi");
+    }
+}
